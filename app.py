9779612d27446b4575693eeea7f018b204cdc217
Michele Acciuffi <micheleacciuffi@gmail.com>
2026-06-04 10:39:59 +0200

    Added new error 403 page

--- app.py
+++ app.py
@@ -528,14 +528,23 @@ def logout():
     return redirect(url_for('landing'))
 
 # ---------------- Error Handlers ----------------
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
+
+@app.errorhandler(401)
+def unauthorized_error(error):
+    flash("Please log in to access this page.", "warning")
+    return redirect(url_for('login'))
+
+@app.errorhandler(403)
+def forbidden_error(error):
+    return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
 
 @app.errorhandler(413)
 def request_entity_too_large(error):
