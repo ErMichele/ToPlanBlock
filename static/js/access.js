99e13bb39537c8cb85b15962c35288840374acfe
Michele Acciuffi <micheleacciuffi@gmail.com>
2026-06-09 23:58:32 +0200

    Implement password visibility toggle in login and registration forms

---
+++ static/js/access.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.password-toggle-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        const input = this.closest('.input-group').querySelector('.password-toggle-input');
+        const icon = this.querySelector('i');
+        
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.remove('bi-eye');
+            icon.classList.add('bi-eye-slash');
+        } else {
+            input.type = 'password';
+            icon.classList.remove('bi-eye-slash');
+            icon.classList.add('bi-eye');
+        }
+    });
+});
